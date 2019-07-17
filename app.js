c48c42c8cace8a920a0ae82fca9d93365af95b0d
szyszak <krzszyszko@gmail.com>
2019-07-17 23:18:32 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// USE CORS
+app.use(cors());
+
+// SERVE STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
+
+// TRACKS INFO
+const tracksData = [
+  {
+    artist: 'Batmobile',
+    album: 'Bail Set as $6.000.000',
+    song: 'Gorilla Rock',
+    url: 'http://localhost:3002/music/gorilla',
+  },
+  {
+    artist: 'Editors',
+    album: 'The Back Room',
+    song: 'Munich',
+    url: 'http://localhost:3002/music/munich',
+  },
+  {
+    artist: 'Blink-182',
+    album: 'Neighborhoods',
+    song: 'Natives',
+    url: 'http://localhost:3002/music/natives',
+  },
+  {
+    artist: 'Calabrese',
+    album: 'Lust for Sacrilege',
+    song: 'Serpentflame',
+    url: 'http://localhost:3002/music/serpentflame',
+  },
+  {
+    artist: 'Miami Horror',
+    album: 'Illumination',
+    song: 'Sometimes',
+    url: 'http://localhost:3002/music/sometimes',
+  },
+];
+
+// SEND INDEX PAGE
+
+// SEND TRACKS INFO
+app.get('/tracks', (req, res) => {
+  res.json(tracksData);
+});
+
+// SEND FILE
+app.get('/music/:trackName', (req, res) => {
+  const { trackName } = req.params;
+  console.log(`SOMEBODY REQUESTED: ${trackName}`);
+  res.set('content-type', 'audio/mp3');
+  res.sendFile(path.join(__dirname, '/public/music', `${trackName}.mp3`));
+});
+
+// START SERVER
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
