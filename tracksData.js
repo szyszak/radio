eee0d7eaa2000c5a491a4c84eca8ec76cfa5a89b
szyszak <krzszyszko@gmail.com>
2019-08-12 21:20:34 +0200

    adding untracked files/folders

---
+++ tracksData.js
@@ -0,0 +1,39 @@
+const tracksData = [
+  {
+    artist: 'Earmake',
+    album: 'Cosmic Hero',
+    song: 'Quantum Gravity (feat. Andromeda Dreams)',
+    URL: 'http://localhost:3001/music/quantum-gravity-feat-andromeda-dreams.mp3',
+    cover: 'http://localhost:3001/covers/earmake-cosmic_hero.jpg',
+  },
+  {
+    artist: 'El Kirpitch',
+    album: 'The Retribution',
+    song: 'Systems Quafilintabilization',
+    URL: 'http://localhost:3001/music/systems-quafilintabilization.mp3',
+    cover: 'http://localhost:3001/covers/el_kirpitch-the_retribution.jpg',
+  },
+  {
+    artist: 'Nightdrive',
+    album: 'Luminance',
+    song: 'Collaborations',
+    URL: 'http://localhost:3001/music/collaborations.mp3',
+    cover: 'http://localhost:3001/covers/nightdrive-luminance.jpg',
+  },
+  {
+    artist: 'Reminiscor',
+    album: 'Reflections',
+    song: 'Space Exploration',
+    URL: 'http://localhost:3001/music/space-exploration.mp3',
+    cover: 'http://localhost:3001/covers/reminiscor-reflections.jpg',
+  },
+  {
+    artist: 'SPACEINVADER',
+    album: 'Escape',
+    song: 'Sequential Movement',
+    URL: 'http://localhost:3001/music/sequential-movement.mp3',
+    cover: 'http://localhost:3001/covers/spaveinvader-escape.jpg',
+  },
+];
+
+module.exports = tracksData;
