1807b4a6e5960863d9e4aceec4edca10ae5fee15
szyszak <krzszyszko@gmail.com>
2019-09-02 20:19:49 +0200

    fixed gitignore

---
+++ public/precache-manifest.c4618f1fdab634d5723db8667f1eca69.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74868865db8035a50386afef9d2d2595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91d9c10dca8f1a6eb07f",
+    "url": "/static/js/2.ce300416.chunk.js"
+  },
+  {
+    "revision": "fa68053302de74f17dcd",
+    "url": "/static/js/main.cf865c3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
